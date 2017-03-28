8e4145474c70c5790c7c6fc134c602c2917f3ecb
Jelena Ristic <jristic611@gmail.com>
2017-03-28 20:52:55 +0200

    no message

--- JSQMessagesViewController/Views/JSQMessagesCollectionViewCellIncoming.m
+++ JSQMessagesViewController/Views/JSQMessagesCollectionViewCellIncoming.m
@@ -30,7 +30,7 @@
     self.cellBottomLabel.textAlignment = NSTextAlignmentLeft;
 
 	self.messageBubbleContainerView.layer.masksToBounds = NO;
-	self.messageBubbleContainerView.layer.shadowOffset = CGSizeMake(0, 10);
+	self.messageBubbleContainerView.layer.shadowOffset = CGSizeMake(0, 5);
 	self.messageBubbleContainerView.layer.shadowRadius = 5;
 	self.messageBubbleContainerView.layer.shadowOpacity = 0.25;
 }
