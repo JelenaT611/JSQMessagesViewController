112a1fb9aed729c0f5ba8997cb0ee24fa91e261c
Jelena Ristic <jristic611@gmail.com>
2017-05-07 20:36:29 +0200

    - fix

--- JSQMessagesViewController/Views/JSQMessagesCollectionViewCell.h
+++ JSQMessagesViewController/Views/JSQMessagesCollectionViewCell.h
@@ -151,8 +151,9 @@ NS_ASSUME_NONNULL_BEGIN
  */
 @property (weak, nonatomic, readonly, nullable) UIImageView *avatarImageView;
 
+@property (weak, nonatomic) IBOutlet UIImageView *statusImageView;
 
-@property (weak, nonatomic, readonly, nullable) UIImageView *statusImageView;
+//@property (weak, nonatomic, nullable) UIImageView *statusImageView;
 
 /**
  *  Returns the avatar container view of the cell. This view is the superview of the cell's avatarImageView.
